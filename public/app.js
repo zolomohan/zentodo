c4832a1bab52ec15320c9a01e4106a7b0af9d681
zolomohan <mohanzolomon@gmail.com>
2019-07-17 04:03:20 -0700

    Toggle todo Status

--- public/app.js
+++ public/app.js
@@ -6,14 +6,20 @@ $(document).ready(function(){
         if(event.which == 13) createTodo();
     })
 
-    $('.list').on('click', 'span', function(){
+    $('.list').on('click', 'span', function(event){
+        event.stopPropagation();
         removeTodo($(this).parent());
+    })
+
+    $('.list').on('click', 'li', function(){
+        toggleTodo($(this));
     })
 })
 
 function addTodo(todo){
     let newTodo =  $('<li class="task">' + todo.todo + '<span>x</span></li>');
     newTodo.data("id", todo._id);
+    newTodo.data("completed", todo.completed);
     if(todo.completed) newTodo.addClass('done');
     $('.list').append(newTodo)
 }
@@ -45,5 +51,22 @@ function removeTodo(todo){
     })
     .catch(function(error){
         console.log(error);
+    })
+}
+
+function toggleTodo(todo){
+    var isDone = !todo.data('completed');
+    var updateData = {completed: isDone};
+    $.ajax({
+        method: 'PUT',
+        url: '/api/todos/'+todo.data('id'),
+        data: updateData
+    })
+    .then(function(){
+        todo.data('completed', isDone);
+        todo.toggleClass('done');
+    })
+    .catch(function(error){
+        console.log(error)
     })
 }
