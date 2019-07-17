d6e7ad5e0e88eff16a2b65c457abda2a60154353
zolomohan <mohanzolomon@gmail.com>
2019-07-17 04:36:01 -0700

    Removed mongo debug mode

--- models/index.js
+++ models/index.js
@@ -1,7 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.set('debug', true);
 mongoose.connect("mongodb://localhost/todo-api", {useNewUrlParser: true});
-
 mongoose.Promise = global.Promise;
 
 module.exports.Todo = require('./todo');
