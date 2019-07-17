65c699a66586625b3e9ed90a2048c6027adb42cd
zolomohan <mohanzolomon@gmail.com>
2019-07-17 03:38:03 -0700

    Delete Todo

--- public/app.js
+++ public/app.js
@@ -5,10 +5,15 @@ $(document).ready(function(){
     $('#todoInput').keypress(function(event){
         if(event.which == 13) createTodo();
     })
+
+    $('.list').on('click', 'span', function(){
+        removeTodo($(this).parent());
+    })
 })
 
 function addTodo(todo){
-    let newTodo =  $('<li class="task">' + todo.todo + '</li>');
+    let newTodo =  $('<li class="task">' + todo.todo + '<span>x</span></li>');
+    newTodo.data("id", todo._id);
     if(todo.completed) newTodo.addClass('done');
     $('.list').append(newTodo)
 }
@@ -27,5 +32,18 @@ function createTodo (){
     })
     .catch(function(error){
         console.log(error); 
+    })
+}
+
+function removeTodo(todo){
+    $.ajax({
+        method: 'DELETE',
+        url: '/api/todos/'+todo.data('id'),
+    })
+    .then(function(){
+        todo.remove(); 
+    })
+    .catch(function(error){
+        console.log(error);
     })
 }
