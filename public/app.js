ebdd98e5151b507cbaf9b42a4733ec555a6c9912
zolomohan <mohanzolomon@gmail.com>
2019-07-17 03:15:10 -0700

    Create Todo

--- public/app.js
+++ public/app.js
@@ -1,12 +1,31 @@
 $(document).ready(function(){
     $.getJSON("/api/todos")
     .then(addTodos)
+
+    $('#todoInput').keypress(function(event){
+        if(event.which == 13) createTodo();
+    })
 })
+
+function addTodo(todo){
+    let newTodo =  $('<li class="task">' + todo.todo + '</li>');
+    if(todo.completed) newTodo.addClass('done');
+    $('.list').append(newTodo)
+}
 
 function addTodos (todos){
     todos.forEach(function(todo){
-        let newTodo =  $('<li class="task">' + todo.todo + '</li>');
-        if(todo.completed) newTodo.addClass('done');
-        $('.list').append(newTodo)
+        addTodo(todo);
+    })
+}
+
+function createTodo (){
+    $.post('/api/todos', {todo: $('#todoInput').val()})
+    .then(function(todo){
+        addTodo(todo);
+        $('#todoInput').val("");
+    })
+    .catch(function(error){
+        console.log(error); 
     })
 }
