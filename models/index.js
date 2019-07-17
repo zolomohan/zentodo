c61190f0504a50e6797ad3c2b98aa21a49c77c1b
ZoloMohan <mohanzolomon@gmail.com>
2019-07-17 20:21:42 +0530

    change db

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://zolomohan:fortmongoknox@campfire-fyj1g.mongodb.net/test?retryWrites=true&w=majority", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://zolomohan:fortmongoknox@campfire-fyj1g.mongodb.net/zenTodo?retryWrites=true&w=majority", {useNewUrlParser: true});
 mongoose.Promise = global.Promise;
 
 module.exports.Todo = require('./todo');
