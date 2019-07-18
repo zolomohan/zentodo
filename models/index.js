514ddfa111c9edce1e3741f5b458c41a3347d097
ZoloMohan <mohanzolomon@gmail.com>
2019-07-18 22:51:02 +0530

    Set Up Environment Variables

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
-const dbCredentials = require('../config/db');
-
 var mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://"+dbCredentials.username+":"+dbCredentials.password+"@campfire-fyj1g.mongodb.net/zenTodo?retryWrites=true&w=majority", {useNewUrlParser: true});
+
+const dburl = process.env.DATABASEURL || "mongodb://localhost/todo-api";
+mongoose.connect(dburl, {useNewUrlParser: true});
 mongoose.Promise = global.Promise;
 
 module.exports.Todo = require('./todo');
