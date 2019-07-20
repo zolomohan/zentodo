cead56cf735e99cad994bf84644717e49a8e3b39
ZoloMohan <mohanzolomon@gmail.com>
2019-07-20 16:40:24 +0530

    mongodb: useFindAndModify set to false

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 let mongoose = require('mongoose');
 
 const dburl = process.env.DATABASEURL || "mongodb://localhost/todo-api";
-mongoose.connect(dburl, {useNewUrlParser: true});
+mongoose.connect(dburl, {useNewUrlParser: true, useFindAndModify: false});
 mongoose.Promise = global.Promise;
 
 module.exports.Todo = require('./todo');
