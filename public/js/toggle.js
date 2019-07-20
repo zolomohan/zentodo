cc0928e2c79e44965ec93bb8cc5f430d5980db85
ZoloMohan <mohanzolomon@gmail.com>
2019-07-20 17:20:33 +0530

    nav mobile flex-column & toggle.js refactor

--- public/js/toggle.js
+++ public/js/toggle.js
@@ -1,21 +1,32 @@
+let form = $('#form'),
+    mainSigninBtn = $('#mainSigninBtn'),
+    signin = $('#option-signin'),
+    signup = $('#option-signup'),
+    confirmpassword = $("#confirmpassword"),
+    authBtn = $('.authBtn.filled'),
+    header = $('header')
+    help = $('#help');
+
 $('.signinBtn').click(function(){
-    $('#form').attr('action', '/login');
-    $('#mainSigninBtn').css('display','none')
-    $("#confirmpassword" ).css('display','none')
-    $("#confirmpassword" ).removeAttr('required')
-    $('#option-signin').css('display','none');
-    $('#option-signup').css('display','block');
-    $('#form').css('display','block');
-    $('.authBtn.filled').text('Sign In')
+    form.attr('action', '/login');
+    [mainSigninBtn, confirmpassword, signin].forEach(function(element){
+        element.css('display','none')
+    });
+    [form, signup].forEach(function(element){
+        element.css('display', 'block');
+    });
+    confirmpassword.removeAttr('required')
+    authBtn.text('Sign In')
 })
 
 $('#altOptionSignup').click(function(){
-    $('#form').attr('action', '/register');
-    $('#form').css('display','block');
-    $("#confirmpassword" ).css('display','inline')
-    $("#confirmpassword" ).attr('required', 'true')
-    $('#mainSigninBtn').css('display','none')
-    $('#option-signup').css('display','none');
-    $('#option-signin').css('display','block');
-    $('.authBtn.filled').text('Sign Up')
+    form.attr('action', '/register');
+    [form, signin].forEach(function(element){
+        element.css('display', 'block');
+    });
+    [signup, mainSigninBtn].forEach(function(element){
+        element.css('display', 'none');
+    });
+    confirmpassword.css('display','inline').attr('required', 'true')
+    authBtn.text('Sign Up')
 })
