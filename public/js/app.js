2d2c5f51318af0dedc95a7cef7de395524707be3
ZoloMohan <mohanzolomon@gmail.com>
2019-07-21 21:29:35 +0530

    Add Loader Text, Toggle Loader

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,4 @@
+let todo;
 $(document).ready(function(){
     $('#todoInput').keypress(function(event){ 
         if(event.which == 13) {
@@ -9,32 +10,34 @@ $(document).ready(function(){
         }
     })
 
-    $('.list').on('click', 'span', function(event){
+    $('.list').on('click', 'span#deleteIcon', function(event){
         event.stopPropagation();
         removeTodo($(this).parent());
         $(this).replaceWith('<span id="loadingContainer"> <span class="lazyloader"></span></span>');
     })
 
     $('.list').on('click', 'li', function(){
+        $(this).children().last().replaceWith('<span id="loadingContainer"> <span class="lazyloader"></span></span>');
         toggleTodo($(this));
     })
 })
 
 function addLoader(){
-    let newTodo =  $('<li class="task"><div class="lazyloader"></div><span><i class="fas fa-trash-alt"></i></span></li>');
-    $('.list').append(newTodo)
+    todo = $('#todoInput').val();
+    $('#todoInput').val("");
+    $('.list').append($(`<li class="task"><div class="lazyloader"></div><span id="loaderTodoContent">${todo}</span></li>`))
 }
 
 function addTodo(todo){
     $('.list').children().last().remove();    
-    let newTodo =  $('<li class="task" id='+todo._id+'>' + todo.todo + '<span><i class="fas fa-trash-alt"></i></span></li>');
+    let newTodo =  $(`<li class="task" id=${todo._id}>${todo.todo}<span id="deleteIcon"><i class="fas fa-trash-alt"></i></span></li>`);
     newTodo.data("completed", todo.completed);
     if(todo.completed) newTodo.addClass('done');
     $('.list').append(newTodo)
 }
 
 function createTodo(){
-    $.post('/api/todos', {todo: $('#todoInput').val()})
+    $.post('/api/todos', {todo: todo})
     .then(function(todo){
         addTodo(todo);
         $('#todoInput').val("");
@@ -69,6 +72,7 @@ function toggleTodo(todo){
     })
     .then(function(){
         todo.toggleClass('done');
+        $(todo).children().last().replaceWith('<span id="deleteIcon"><i class="fas fa-trash-alt"></i></span>');
     })
     .catch(function(error){
         console.log(error)
